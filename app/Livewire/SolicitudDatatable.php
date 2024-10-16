ebdff0b4f1e11a958ce5c243c257d2d8a13624d1
Jonathan-C117 <jonathan@nomaddi.com>
2024-10-16 08:37:32 -0500

    visualizar nombre e imagen en el campo de archivos

--- app/Livewire/SolicitudDatatable.php
+++ app/Livewire/SolicitudDatatable.php
@@ -13,7 +13,6 @@ class SolicitudDatatable extends DataTableComponent
 {
     protected $model = Solicitud::class;
     protected $listeners = ['Updated' => '$refresh']; // Refrescar la tabla cuando se actualiza un tenant
-
     public ?int $searchFilterDebounce = 600;
     public array $perPageAccepted = [10, 20, 50, 100];
 
@@ -23,7 +22,22 @@ class SolicitudDatatable extends DataTableComponent
         $this->setDefaultSort('id', 'desc');
         $this->setSingleSortingStatus(false);
     }
-
+    public function getIconByExtension($file)
+    {
+        $extension = pathinfo($file, PATHINFO_EXTENSION);
+        switch ($extension) {
+            case 'pdf':
+                return 'fa-file-pdf';
+            case 'doc':
+            case 'docx':
+                return 'fa-file-word';
+            case 'xls':
+            case 'xlsx':
+                return 'fa-file-excel';
+            default:
+                return 'fa-file';
+        }
+    }
     public function columns(): array
     {
         return [
@@ -33,13 +47,10 @@ class SolicitudDatatable extends DataTableComponent
             Column::make("Usuario", "user.name")
                 ->sortable()
                 ->searchable(),
-
-
             Column::make("Número de Identificación", "numeroIdentificacion")
                 ->sortable()
                 ->searchable()
                 ->collapseOnMobile(),
-
             Column::make("Barrio", "barrio.nombreBarrio")
                 ->sortable()
                 ->searchable() // Relacionado con la tabla de barrios
@@ -48,13 +59,21 @@ class SolicitudDatatable extends DataTableComponent
                 ->sortable()
                 ->searchable() // Relacionado con la tabla de direcciones
                 ->collapseOnMobile(),
-            Column::make("Evidencia PDF", "evidenciaPDF")
-                ->label(
-                    fn($row) => $row->evidenciaPDF
-                    ? '<a href="' . Storage::url($row->evidenciaPDF) . '" target="_blank" class="text-blue-500">Ver archivo</a>'
-                    : 'No disponible'
-                )
-                ->html()
+            Column::make("Anexos", "evidenciaPDF")
+                ->format(function ($value, $row) {
+                    $files = json_decode($value); // Convierte el valor JSON a un array
+                    if (is_array($files)) {
+                        return implode(' ', array_map(function ($file) {
+                            $icon = $this->getIconByExtension($file);
+                            $fileName = basename($file); // Obtener el nombre del archivo
+                            return "<a href='/storage/$file' target='_blank' title='$fileName'><i class='fas $icon fa-2x' style='color: blue';></i> $fileName</a>";
+                        }, $files));
+                    }
+                    return 'Sin archivos';
+                })
+                ->html() // Importante: Permite la interpretación del HTML en la columna
+                ->sortable()
+                ->searchable()
                 ->collapseOnMobile(),
             Column::make("Created at", "created_at")
                 ->sortable()
