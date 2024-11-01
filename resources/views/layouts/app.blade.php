f59c3823ad497b672a7119a4b7ab9faef3332e10
jhonrymat <jhonrymat@gmail.com>
2024-10-31 21:49:12 -0500

    arreglando errores menos el de input select

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,6 @@
         <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
         <style>
             [x-cloak] {
@@ -46,7 +45,7 @@
         </div>
 
         @stack('modals')
-
+        {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
         @livewireScripts
     </body>
 </html>
