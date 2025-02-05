3e63e2fd23d14d69485aa5930b0f9d2d29331398
jhonrymat <jhonrymat@gmail.com>
2025-02-05 13:25:20 -0500

    cambios en transcurso de la mañana 05-02-2025

--- app/Livewire/FormularioComponent.php
+++ app/Livewire/FormularioComponent.php
@@ -65,7 +65,7 @@ class FormularioComponent extends Component
         'electoral' => 'file|mimes:pdf,jpeg,jpg|max:10240', // Valida cada archivo individualmente
         'sisben' => 'file|mimes:pdf,jpeg,jpg|max:10240', // Valida cada archivo individualmente
         'cedula' => 'file|mimes:pdf,jpeg,jpg|max:10240', // Valida cada archivo individualmente
-        'recibo' => 'file|mimes:pdf,jpeg,jpg|max:10240', // Valida cada archivo individualmente
+        'recibo' => 'required|file|mimes:pdf,jpeg,jpg|max:10240', // Valida cada archivo individualmente
         'terminos' => 'required',
         'observaciones' => 'nullable|string',
     ];
@@ -87,6 +87,7 @@ class FormularioComponent extends Component
         'cedula.max' => 'El campo evidencia no debe ser mayor a 10MB.',
         'recibo.mimes' => 'El campo evidencia debe ser un archivo de tipo: pdf, jpeg, jpg',
         'recibo.max' => 'El campo evidencia no debe ser mayor a 10MB.',
+        'recibo.required' => 'El campo recibo es obligatorio.',
         'terminos.required' => 'El campo términos es obligatorio.',
         'observaciones.string' => 'El campo observaciones debe ser una cadena de texto.',
     ];
