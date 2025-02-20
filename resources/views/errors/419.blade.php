10378d4a0890ffc5d7c3770a692a22eb6efccfd9
jhonrymat <jhonrymat@gmail.com>
2025-02-20 10:13:13 -0500

    vista mantenimiento y centrado de otras vistas de errores

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -4,27 +4,24 @@
             {{ __('Página Expirada') }}
         </h2>
     </x-slot>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
-                    <div class="text-center p-6 rounded-lg">
-                        <!-- Logo -->
-                        <x-application-logo class="w-20 h-20 mx-auto mb-4" />
 
-                        <!-- Error Code -->
-                        <h1 class="text-4xl font-bold text-red-500">419</h1>
-                        <p class="mt-4 text-gray-700 text-lg font-semibold">Tu sesión ha expirado</p>
-                        <p class="mt-2 text-gray-500">Por razones de seguridad, tu sesión ha expirado. Por favor, inicia sesión nuevamente para continuar.</p>
+    <div class="flex items-center justify-center min-h-screen bg-gray-100">
+        <div class="bg-white shadow-xl sm:rounded-lg p-8 text-center w-full max-w-3xl">
+            <!-- Logo -->
+            <x-application-logo class="w-20 h-20 mx-auto mb-4" />
 
-                        <!-- Botón para redirigir al login -->
-                        <a href="{{ route('login') }}"
-                            class="mt-6 inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors">
-                            Iniciar Sesión
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <!-- Error Code -->
+            <h1 class="text-4xl font-bold text-red-500">419</h1>
+            <p class="mt-4 text-gray-700 text-lg font-semibold">Tu sesión ha expirado</p>
+            <p class="mt-2 text-gray-500">
+                Por razones de seguridad, tu sesión ha expirado. Por favor, inicia sesión nuevamente para continuar.
+            </p>
+
+            <!-- Botón para redirigir al login -->
+            <a href="{{ route('login') }}"
+                class="mt-6 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors">
+                Iniciar Sesión
+            </a>
         </div>
     </div>
 </x-guest-layout>
